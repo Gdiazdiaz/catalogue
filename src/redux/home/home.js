33e942cd9c46845772a67d71773e3f09b43829cb
Glenda Diaz <glendadiazah@outlook.com>
2023-02-09 11:35:55 -0600

    Add linter files and fix errors

--- src/redux/home/home.js
+++ src/redux/home/home.js
@@ -1,38 +1,38 @@
 const DATA_LIST = 'catalogue/src/redux/home/home/DATA_LIST';
-const API_URL ='https://api.disneyapi.dev/characters/';
+const API_URL = 'https://api.disneyapi.dev/characters/';
 
-const homeReducer = (state=[], action) => {
-    switch (action.type) {
-        case DATA_LIST:
-            return action.dataList;
-        default:
-            return state;
-    }
-}
+const homeReducer = (state = [], action) => {
+  switch (action.type) {
+    case DATA_LIST:
+      return action.dataList;
+    default:
+      return state;
+  }
+};
 
 const getData = async () => {
-    try {
-      const result = await fetch(API_URL);
-      const data = await result.json();
-      const data2 = data.data;
-      for(let i=1; i<data2.length; i+=4){
-        data2[i].index = "color";
+  try {
+    const result = await fetch(API_URL);
+    const data = await result.json();
+    const data2 = data.data;
+    for (let i = 1; i < data2.length; i += 4) {
+      data2[i].index = 'color';
     }
-    for(let j=2; j<data2.length; j+=4){
-      data2[j].index = "color";
+    for (let j = 2; j < data2.length; j += 4) {
+      data2[j].index = 'color';
     }
     return data2;
-    } catch (error) {
-      return error;
-    }
-  };
+  } catch (error) {
+    return error;
+  }
+};
 
 export const dataList = () => async (dispatch) => {
-    const dataList = await getData();
-    dispatch({
-        type: DATA_LIST,
-        dataList,
-    });
+  const dataList = await getData();
+  dispatch({
+    type: DATA_LIST,
+    dataList,
+  });
 };
 
 export default homeReducer;
